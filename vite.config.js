ad86d77e098afa7d749da92e2a2794dd3c9bb862
rambacs <bence.rambacs@gmail.com>
2025-09-18 00:10:12 +0200

    config changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/vending-machine/", // replace with your repository name
+  base: "/", // replace with your repository name
 })
